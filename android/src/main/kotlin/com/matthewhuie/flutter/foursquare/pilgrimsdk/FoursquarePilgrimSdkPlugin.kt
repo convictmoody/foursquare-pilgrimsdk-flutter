f6554813998272c797372228c022a1ef74536087
Matthew Huie <matthew@matthewhuie.com>
2018-10-10 17:14:46 -0400

    Working SDK initialization on Android

--- android/src/main/kotlin/com/matthewhuie/flutter/foursquare/pilgrimsdk/FoursquarePilgrimSdkPlugin.kt
+++ android/src/main/kotlin/com/matthewhuie/flutter/foursquare/pilgrimsdk/FoursquarePilgrimSdkPlugin.kt
@@ -1,21 +1,60 @@
 package com.matthewhuie.flutter.foursquare.pilgrimsdk
 
+import android.content.Context
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import com.foursquare.pilgrim.*
 
-class FoursquarePilgrimSdkPlugin(): MethodCallHandler {
+class FoursquarePilgrimSdkPlugin: MethodCallHandler {
+
   companion object {
+    private lateinit var registrar: Registrar
+
     @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
+    fun registerWith(registrar: Registrar) {
       val channel = MethodChannel(registrar.messenger(), "foursquare_pilgrimsdk")
       channel.setMethodCallHandler(FoursquarePilgrimSdkPlugin())
+      this.registrar = registrar
+
+      PilgrimSdk.with(
+        PilgrimSdk.Builder(registrar.context())
+          .consumer("", "")
+          .logLevel(LogLevel.DEBUG)
+          .notificationHandler(object : PilgrimNotificationHandler() {
+            override fun handleVisit(c: Context, psvn: PilgrimSdkVisitNotification) {
+              val venue = psvn.visit.toString()
+              android.util.Log.d("foursquare-pilgrimsdk", venue)
+            }
+          })
+      )
     }
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result): Unit {
-    result.notImplemented()
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    call.apply {
+      when (call.method) {
+        "start" -> {
+          PilgrimSdk.start(registrar.context())
+        }
+        "stop" -> {
+          PilgrimSdk.stop(registrar.context())
+        }
+        "fireTestVisit" -> {
+          PilgrimNotificationTester.fireTestVisit(registrar.context(), Confidence.HIGH, LocationType.VENUE, false)
+        }
+        "getInstallId" -> {
+          result.success(PilgrimSdk.getInstallId())
+        }
+        "isEnabled" -> {
+          result.success(PilgrimSdk.isEnabled())
+        }
+        else -> {
+          result.notImplemented()
+        }
+      }
+    }
   }
 }
--- lib/foursquare_pilgrimsdk.dart
+++ lib/foursquare_pilgrimsdk.dart
@@ -1,6 +1,27 @@
+import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FoursquarePilgrimSdk {
   static const MethodChannel _channel =
       const MethodChannel('foursquare_pilgrimsdk');
+
+  static start() {
+    _channel.invokeMethod("start");
+  }
+
+  static stop() {
+    _channel.invokeMethod("stop");
+  }
+
+  static fireTestVisit() {
+    _channel.invokeMethod("fireTestVisit");
+  }
+
+  static Future<String> get installId async {
+    return await _channel.invokeMethod("getInstallId");
+  }
+
+  static dynamic get isEnabled async {
+    return await _channel.invokeMethod("isEnabled");
+  }
 }
