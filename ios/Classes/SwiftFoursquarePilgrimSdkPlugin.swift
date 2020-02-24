e5cc38d38e4d46059f8a48c9895fb265a1c42c6d
Muhammad <muhammad@polarstork.com>
2020-02-24 17:44:59 +0200

    Init Configuration

--- ios/Classes/SwiftFoursquarePilgrimSdkPlugin.swift
+++ ios/Classes/SwiftFoursquarePilgrimSdkPlugin.swift
@@ -11,10 +11,12 @@ public class SwiftFoursquarePilgrimSdkPlugin: NSObject, FlutterPlugin, FlutterSt
     let instance = SwiftFoursquarePilgrimSdkPlugin()
     registrar.addMethodCallDelegate(instance, channel: fmc)
     fec.setStreamHandler(instance)
-    
+//     let consumerKey = Bundle.main.object(forInfoDictionaryKey: "PILGRIM_CONSUMER_KEY") as? String
+//     let secret = Bundle.main.object(forInfoDictionaryKey: "PILGRIM_SECRET") as? String
+
     PilgrimManager.shared().configure(
-        withConsumerKey: "",
-        secret: "",
+        withConsumerKey: "NYL5R1DDYA3QWP3RCW2XM4PSFRJ4TLB5JOFRX4ODHKP1ID1D",
+        secret: "IE1LH4NO2KNNG1XSIQH3UZWOSOLRK31GT1ELMIZZYVXKWWNJ",
         delegate: instance,
         completion: nil)
   }
